485ae19e41fa1b75a92a15e30c378d8b20b3791b
Justin Navarro <info@justinnavarro.net>
2026-01-04 14:54:31 -0500

    setup adonis drive

---
+++ config/drive.ts
@@ -0,0 +1,14 @@
+import app from '@adonisjs/core/services/app'
+import env from '#start/env'
+import { defineConfig, services } from '@adonisjs/drive'
+
+export default defineConfig({
+  default: env.get('DRIVE_DISK', 'fs') as 'fs',
+
+  services: {
+    fs: services.fs({
+      location: app.makePath('storage'),
+      visibility: 'private',
+    }),
+  },
+})
